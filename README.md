7894369fc68b8c14550dffe4e73aa010c72260e6
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-22 10:49:12 +0900

    sveltekit work app - first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SvelteKit Work
+SvelteKit + Vite + Flowbite + Tailwind
+
+# Creating Project
+The project is created by [flowbite-svelte](https://github.com/themesberg/flowbite-svelte)
---
+++ app/vite.config.ts
@@ -0,0 +1,13 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	server: {
+    host: '0.0.0.0',
+		port: 8085
+  },
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
