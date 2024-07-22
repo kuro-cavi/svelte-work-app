5be876cf4db365daee6fb2f930a4b154d5d595fd
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-22 15:46:40 +0900

    formatt

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,12 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	server: {
+  server: {
     host: '0.0.0.0',
-		port: 8085
+    port: 8085
   },
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
