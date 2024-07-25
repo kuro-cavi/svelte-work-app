382b94d919b00464716685894d3a44fe5ce5d5f2
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-25 18:50:19 +0900

    update sveltekit setting

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  <title>Error</title>
+  <meta name="description" content="Error Page" />
+</svelte:head>
+
+<div class="flex h-[calc(100vh-4rem)] items-center justify-center">
+  <p>{$page.error?.message ?? 'Error'}</p>
+</div>
