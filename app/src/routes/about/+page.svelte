734f2087ac45997302b7a9182871dbf3b4c31b55
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-25 18:55:17 +0900

    about

---
+++ app/src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>About</title>
+  <meta name="description" content="About Page" />
+</svelte:head>
+
+<div class="h-[calc(100vh-4rem)] flex items-center justify-center">
+  <p>about</p>
+</div>
